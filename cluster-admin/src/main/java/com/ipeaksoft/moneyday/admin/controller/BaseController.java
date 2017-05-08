83ef79d4025591d76a49e75114db512c22a6cb8e
wangyijinccx <897168791@qq.com>
2017-05-08 09:59:41 +0800

    优化

--- cluster-admin/src/main/java/com/ipeaksoft/moneyday/admin/controller/BaseController.java
+++ cluster-admin/src/main/java/com/ipeaksoft/moneyday/admin/controller/BaseController.java
@@ -11,27 +11,27 @@ import com.ipeaksoft.moneyday.core.service.AdminUserService;
 public class BaseController {
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	public String host ="localhost";
+	public String host ="localhost/dms";
 	// 获得所有设备列表
-	public String devices = "http://"+host+"/dms/devices";
+	public String devices = "http://"+host+"/devices";
 	// 得到所有的脚本列表
-	public String scripts = "http://"+host+"/dms/scripts";
+	public String scripts = "http://"+host+"/scripts";
 	// 得到某个游戏有哪些脚本可以跑
-	public String game_scripts = "http://"+host+"/dms/game_scripts?id=%s&version=%s&platform=%s";
+	public String game_scripts = "http://"+host+"/game_scripts?id=%s&version=%s&platform=%s";
 	// 重启一个设备
-	public String reboot_device = "http://"+host+"/dms/reboot_device?udid=%s";
+	public String reboot_device = "http://"+host+"/reboot_device?udid=%s";
 	// 执行脚本
-	public String run_script = "http://"+host+"/dms/run_script"
+	public String run_script = "http://"+host+"/run_script"
 			+ "?udid=%s&internal_id=%s&script_id=%s&game_id=%s"
 			+ "&username=%s&password=%s&server=%s&time=%s&recover=%s";
 	//强行停止当前脚本
-	public String stop_script="http://"+host+"/dms/stop_script?udid=%s";
+	public String stop_script="http://"+host+"/stop_script?udid=%s";
 	//强行停止所有脚本
-	public String stop_all_scripts = "http://"+host+"/dms/stop_all_scripts?udid=%s";
+	public String stop_all_scripts = "http://"+host+"/stop_all_scripts?udid=%s";
 	//暂停当前脚本
-	public String pause_script = "http://"+host+"/dms/pause_script?udid=%s";
+	public String pause_script = "http://"+host+"/pause_script?udid=%s";
 	//继续当前脚本
-	public String resume_script = "http://"+host+"/dms/resume_script?udid=%s";
+	public String resume_script = "http://"+host+"/resume_script?udid=%s";
 
 	
 	@Autowired
